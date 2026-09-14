753bbe49affaa6a49f477d6f6c9173168a124105
Châu Đan Huy <chauhuy781thd@gmail.com>
2026-09-14 21:57:03 +0700

    upgrade v4

--- functions/api/auth/me.js
+++
@@ -1,8 +0,0 @@
-import { getAuthenticatedMember } from '../../_lib/auth.js';
-
-export async function onRequestGet({ request, env }) {
-  const member = await getAuthenticatedMember(request, env);
-  return Response.json({ authenticated: !!member, member: member || null }, {
-    headers: { 'Cache-Control': 'no-store' }
-  });
-}
